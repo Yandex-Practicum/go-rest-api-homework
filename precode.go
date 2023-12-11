96e5f5d455e75a153477b0017b475f284fd3984e
anburbaeva <an.burbaeva@gmail.com>
2023-12-11 22:09:34 +0600

    Я сделала это!

--- precode.go
+++ precode.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -40,13 +41,64 @@ var tasks = map[string]Task{
 }
 
 // Ниже напишите обработчики для каждого эндпоинта
-// ...
+func getAllTasksHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func createTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var newTask Task
+	err := json.NewDecoder(r.Body).Decode(&newTask)
+	if err != nil {
+		http.Error(w, "Ошибка при декодировании JSON", http.StatusBadRequest)
+		return
+	}
+
+	newTask.ID = fmt.Sprint(len(tasks) + 1)
+
+	tasks[newTask.ID] = newTask
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getTaskByIDHandler(w http.ResponseWriter, r *http.Request) {
+	taskID := chi.URLParam(r, "id")
+
+	task, ok := tasks[taskID]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+}
+
+func deleteTaskByIDHandler(w http.ResponseWriter, r *http.Request) {
+	taskID := chi.URLParam(r, "id")
+
+	_, ok := tasks[taskID]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+
+	delete(tasks, taskID)
+
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getAllTasksHandler)
+	r.Post("/tasks", createTaskHandler)
+	r.Get("/tasks/{id}", getTaskByIDHandler)
+	r.Delete("/tasks/{id}", deleteTaskByIDHandler)
+
+	port := ":8080"
+	fmt.Printf("Сервер запущен на порту %s\n", port)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
