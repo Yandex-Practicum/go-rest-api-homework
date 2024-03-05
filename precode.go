a043ce88338db30b0ba45c8a8478f292194b1129
Roman Semenkov <nicedayone7@gmail.com>
2024-03-05 23:47:31 +0300

    Add routs getTasks addTask getOneTask delTask

--- precode.go
+++ precode.go
@@ -46,7 +46,10 @@ func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getTasks)
+	r.Post("/tasks", addTask)
+	r.Get("/tasks/{id}", getOneTask)
+	r.Delete("/tasks/{id}", delTask)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
