d23ec580bcee1de8aa6157c6126cbe5deca935cc
Павел <lopatinpavel7@yandex.ru>
2024-03-12 01:47:05 +0300

    My first commit

--- precode.go
+++ precode.go
@@ -46,7 +46,6 @@ func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
-	// ...
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
